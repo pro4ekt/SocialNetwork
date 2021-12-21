743b03c5ffb43f02bd851ce880a01b97f4cd5267
Yevhen Horbokon <zhekaprobro@gmail.com>
2021-12-21 18:32:29 +0200

    DAL

---
+++ DAL/EF/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace DAL.EF
+{
+    public class ApplicationContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationContext(string conectionString) : base(conectionString) { }
+
+        public DbSet<ClientProfile> ClientProfiles { get; set; }
+    }
+}
---
+++ DAL/Identity/ApplicationUserManager.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Entities;
+using Microsoft.AspNet.Identity;
+
+namespace DAL.Identity
+{
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+            : base(store)
+        {
+        }
+    }
+}
